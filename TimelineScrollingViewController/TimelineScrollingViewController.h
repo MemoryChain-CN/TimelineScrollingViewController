1eeacca3a82572ffce05a3a74e6ae09a8d49cdf3
Felipe Ricieri <felipericieri@icloud.com>
2018-08-23 20:03:24 -0300

    Setup

---
+++ TimelineScrollingViewController/TimelineScrollingViewController.h
@@ -0,0 +1,19 @@
+//
+//  TimelineScrollingViewController.h
+//  TimelineScrollingViewController
+//
+//  Created by Felipe Ricieri on 23/08/2018.
+//  Copyright © 2018 Ricieri ME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TimelineScrollingViewController.
+FOUNDATION_EXPORT double TimelineScrollingViewControllerVersionNumber;
+
+//! Project version string for TimelineScrollingViewController.
+FOUNDATION_EXPORT const unsigned char TimelineScrollingViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimelineScrollingViewController/PublicHeader.h>
+
+
